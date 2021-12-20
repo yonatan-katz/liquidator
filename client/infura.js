22a782b1bd9891d9c8b98452ceb215060bb4cc07
amir <amir@semizi.com>
2021-12-20 02:02:59 +0200

    Monitor accounts from input file. Trigger health update on every price movement and periodiclly (top n).

--- client/infura.js
+++ client/infura.js
@@ -3,9 +3,6 @@ const LendingPoolV2Artifact = require("@aave/protocol-v2/artifacts/contracts/pro
 const abi = require("./abi.js");
 const chainlink = require("./chainlink.js");
 
-// Log the ABI into console
-//console.log(LendingPoolV2Artifact.abi);
-
 const { ethers } = require("ethers");
 
 const uniswap_anchored_view_address =
@@ -18,7 +15,7 @@ const aave_lending_pool_v2_address =
 const aave_incentive_controller_address =
   "0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5";
 
-const chainlink_aggregator_address =
+const chainlink_aggregator_address_eth_usd =
   "0x37bC7498f4FF12C19678ee8fE19d713b87F6a9e6";
 
 //const projectId = "fd3ac79f46ba4500be8e92da9632b476"; //Yonatan
@@ -27,21 +24,114 @@ const projectId = "474dfacad06547a4aba817b93fa852c9";
 const tstamp = () => new Date(Date.now()).toISOString();
 
 const toPrice = (big_number, coin_pair) => {
-    if(coin_pair.includes("/USD")) {
-	const denom = 1e8;
-	return ethers.BigNumber.from(big_number).toNumber()/ denom;
-    }
-    return ethers.utils.formatEther(big_number.toString());
+  if (coin_pair.includes("/USD")) {
+    const denom = 1e8;
+    return ethers.BigNumber.from(big_number).toNumber() / denom;
+  }
+  return ethers.utils.formatEther(big_number.toString());
 };
 
+const loadAccounts = (json_file) => {
+  console.log("loading tracked accounts from %s", json_file);
+  var accounts = require(json_file);
 
-async function main() {
-    
-    console.log("%s running infura client", tstamp());
-    provider = ethers.providers.InfuraProvider.getWebSocketProvider(
-	"homestead",
-	projectId
+  console.log("tracked accounts:");
+  for (account of accounts) {
+    account.health = 0;
+    console.log(
+      "%s collateral:%s debt:%s",
+      account.account,
+      account.collateralAssets,
+      account.debtAssets
     );
+  }
+
+  return accounts;
+};
+
+//
+//Fetch account health
+//
+const fetchAccountState = async (account, aave_lending_pool_contract) => {
+  var prev_hfactor = account.health;
+  const account_data = await aave_lending_pool_contract.getUserAccountData(
+    account.account
+  );
+
+  var current_hfactor = account_data["healthFactor"] / 1e18;
+
+  console.log(
+    "%s %s health factor: %s %s %s (%f)",
+    tstamp(),
+    account.account,
+    account.collateralAssets,
+    account.debtAssets,
+    current_hfactor,
+    current_hfactor - prev_hfactor
+  );
+
+  account.health = current_hfactor;
+};
+
+//
+//Fetch accounts by coin pair and query their health
+//
+const fetchAccountsStateByCoinPair = async (
+  accounts,
+  coin_pair,
+  aave_lending_pool_contract
+) => {
+  if (!coin_pair.includes("/ETH")) {
+    //ignore none ETH based currencies
+    return;
+  }
+
+  const coin = coin_pair.split("/")[0];
+  for (account of accounts) {
+    if (
+      account.collateralAssets.includes(coin) ||
+      account.debtAssets.includes(coin)
+    ) {
+      await fetchAccountState(account, aave_lending_pool_contract);
+    }
+  }
+};
+
+//
+//Top N accounts - i.e. accounts with lowest health factor
+//
+const topNAccounts = (accounts, N) => {
+  accounts.sort((a, b) => {
+    return a.health - b.health;
+  });
+
+  return accounts.slice(0, N);
+};
+
+//
+//Main
+//
+async function main() {
+  const tracked_accounts_file = process.argv[2];
+  if (!tracked_accounts_file) {
+    console.log("missing argument - tracked files json file");
+    return;
+  }
+
+  //Running modes
+  const fast_init = false;
+  const test_mode = false;
+  const monitor_blocks = false;
+  const monitor_pending_tx = false;
+
+  //Load tracked accounts
+  var tracked_accounts = loadAccounts(tracked_accounts_file);
+
+  console.log("%s running infura client", tstamp());
+  provider = ethers.providers.InfuraProvider.getWebSocketProvider(
+    "homestead",
+    projectId
+  );
 
   block_number = await provider.getBlockNumber();
   console.log("%s connected to infura, last block %d", tstamp(), block_number);
@@ -54,6 +144,23 @@ async function main() {
     aave_lending_pool_v2_abi,
     provider
   );
+
+  //
+  //Fetch current accounts state
+  //
+  if (!fast_init) {
+    for (account of tracked_accounts) {
+      await fetchAccountState(account, aave_lending_pool_contract);
+    }
+  }
+
+  if (test_mode) {
+    await fetchAccountsStateByCoinPair(
+      tracked_accounts,
+      "AAVE/ETH",
+      aave_lending_pool_contract
+    );
+  }
 
   /* await aave_lending_pool_contract.connect(provider);
      * aave_lending_pool_contract.on("Borrow", (...evt) => {
@@ -81,17 +188,9 @@ async function main() {
   /* aave_lending_pool_contract.on("Repay", (...evt) => {
    *   console.log("%s lending pool repay  %s", tstamp(), evt);
    * }); */
-  /*
-   *   const evts = await aave_lending_pool_contract.queryFilter(
-   *     "LiquidationCall",
-   *     block_number - 1000
-   *   );
-   *   for (const evt of evts) {
-   *     console.log("%s %s", tstamp(), evt);
-   *   } */
 
   //
-  //Price feed contract
+  //Price feed contract - uniswap
   //
   const price_feed_contract = new ethers.Contract(
     uniswap_anchored_view_address,
@@ -116,101 +215,19 @@ async function main() {
     "SUSHI",
     "USDC",
   ]) {
-    if (1) {
-      const config = await price_feed_contract.getTokenConfigBySymbol(symbol);
-      symbol_map[config["symbolHash"]] = symbol;
+    const config = await price_feed_contract.getTokenConfigBySymbol(symbol);
+    symbol_map[config["symbolHash"]] = symbol;
 
+    if (!fast_init) {
       const price = await price_feed_contract.price(symbol);
-      console.log("%s, %s/USD price: %s", tstamp(), symbol, price.toNumber() / 1e6);
+      console.log(
+        "%s, %s/USD price: %s",
+        tstamp(),
+        symbol,
+        price.toNumber() / 1e6
+      );
     }
   }
-
-  //console.log(symbol_map);
-
-  //Query events in previous blocks
-  /* const evts = await price_feed_contract.queryFilter(
-   *   "PriceUpdated",
-   *   13744592 - 50,
-   *   13744592 + 2
-   * );
-   * for (const evt of evts) {
-   *   console.log("%s %s", tstamp(), evt);
-   *   console.log(
-   *     "%s price %s %f",
-   *     tstamp(),
-   *     symbol_map[evt["topics"][0]],
-   *     ethers.BigNumber.from(evt["data"]).toNumber() / 1e6
-   *   );
-   * } */
-
-    
-    //
-    //Chainlink price oracles
-    //
-    const chainlink_oracles = {};
-  for (const [contract, coin_pair] of Object.entries(chainlink.price_oracles)) {
-    const oracle = new ethers.Contract(contract, abi.chainlink_abi, provider);
-
-    oracle.on("AnswerUpdated", (...evt) => {
-      var price = toPrice(evt[0], coin_pair);
-      console.log(
-        "%s %s chainlink price update %s %f",
-        tstamp(),
-        evt[3]["blockNumber"],
-        coin_pair,
-        price
-      );
-    });
-
-    chainlink_oracles[coin_pair] = oracle;
-
-    const latest_answer = await oracle.latestAnswer();
-    console.log(
-	"%s registering chainlink price oracle %s %s",
-	tstamp(),
-	coin_pair,
-	toPrice(latest_answer, coin_pair)
-    );
-  }
-
-  const chainlink_eth_usd_contract = new ethers.Contract(
-    chainlink_aggregator_address,
-    abi.chainlink_abi,
-    provider
-  );
-
-  //Query events in previous blocks
-  /* const evts = await chainlink_eth_usd_contract.queryFilter(
-   *   "AnswerUpdated",
-   *   13744592 - 1000,
-   *   13744592 + 2
-   * );
-   * for (const evt of evts) {
-   *   //console.log("%s %s", tstamp(), evt);
-   *   console.log(
-   *     "%s %d chainlink eth/usd price %f",
-   *     tstamp(),
-   *     evt["blockNumber"],
-   *     ethers.BigNumber.from(evt["topics"][1]).toNumber() / 1e6
-   *   );
-   * } */
-
-  /* chainlink_eth_usd_contract.on("AnswerUpdated", (...evt) => {
-   *   var price = ethers.BigNumber.from(evt[0]).toNumber() / 1e8;
-   *   console.log(
-   *     "%s %s chainlink price update (AnswerUpdated) %f",
-   *     tstamp(),
-   *     evt[3]["blockNumber"],
-   *     price
-   *   );
-   *   for (const _e of evt) {
-   *     console.log("%s", _e);
-   *   }
-   * }); */
-
-  /* chainlink_eth_usd_contract.on("NewRound", (...evt) => {
-   *   console.log("%s %s chainlink NewRound", tstamp(), evt[3]["blockNumber"]);
-   * }); */
 
   price_feed_contract.on("PriceUpdated", (...evt) => {
     const symbol = symbol_map[evt[0]];
@@ -226,9 +243,53 @@ async function main() {
   });
 
   //
+  //Chainlink price oracles
+  //
+  const chainlink_oracles = {};
+  for (const [contract, coin_pair] of Object.entries(chainlink.price_oracles)) {
+    const oracle = new ethers.Contract(contract, abi.chainlink_abi, provider);
+
+    oracle.on("AnswerUpdated", (...evt) => {
+      var price = toPrice(evt[0], coin_pair);
+      console.log(
+        "%s %s chainlink price update %s %f",
+        tstamp(),
+        evt[3]["blockNumber"],
+        coin_pair,
+        price
+      );
+
+      //Recalc accounts health for accounts that hold this coin
+      fetchAccountsStateByCoinPair(
+        tracked_accounts,
+        coin_pair,
+        aave_lending_pool_contract
+      );
+    });
+
+    chainlink_oracles[coin_pair] = oracle;
+
+    if (!fast_init) {
+      const latest_answer = await oracle.latestAnswer();
+      console.log(
+        "%s registering chainlink price oracle %s %s",
+        tstamp(),
+        coin_pair,
+        toPrice(latest_answer, coin_pair)
+      );
+    }
+  }
+
+  //'special' contract eth/usd price
+  const chainlink_eth_usd_contract = new ethers.Contract(
+    chainlink_aggregator_address_eth_usd,
+    abi.chainlink_abi,
+    provider
+  );
+
+  //
   // Register for pending transactions
   //
-  const monitor_pending_tx = false;
   if (monitor_pending_tx) {
     provider.on("pending", (tx) => {
       //console.log("%s pending tx: %s", tstamp(), tx);
@@ -238,14 +299,11 @@ async function main() {
     });
   }
 
-  const monitor_blocks = false;
   if (monitor_blocks) {
     provider.on("block", (tx) => {
       console.log("%s block: %s", tstamp(), tx);
     });
   }
-
-  //var prev_hfactor = 0.0;
 
   console.log("%s begin listening to live events", tstamp());
 
@@ -261,26 +319,15 @@ async function main() {
       eth_usd_price
     );
 
-    /* const tracked_account = "0xc65ee4f20226fF0f7AA546a368036E640c57D3BE";
-       * const account_data = await aave_lending_pool_contract.getUserAccountData(
-       *   tracked_account
-       * );
-
-       * var current_hfactor = account_data["healthFactor"] / 1e18;
-       * console.log(
-       *   "%s %s health factor: %s (%f)",
-       *   tstamp(),
-       *   tracked_account,
-       *   current_hfactor,
-       *   current_hfactor - prev_hfactor
-	   * );
-      
-
-       * prev_hfactor = current_hfactor; */
-
     //Sleep for a while - listening to events in background
     const MIN = 60000;
     await new Promise((res) => setTimeout(() => res(null), 5 * MIN));
+
+    //Query state of top N accounts i.e. nearest liquidation
+    const top_n = topNAccounts(tracked_accounts, 10);
+    for (account of top_n) {
+      await fetchAccountState(account, aave_lending_pool_contract);
+    }
   }
 }
 
